0f8e673bede5c077ba586f496bab01e407e66a61
AiceKerime <rafiizzaturohman06@gmail.com>
2022-12-10 02:41:37 +0700

    React

---
+++ views/src/components/UserBox.js
@@ -0,0 +1,14 @@
+export default function UserBox() {
+    return (
+        <div className="mx-20 my-10">
+            <div className="border border-black rounded-md">
+                <div className="">
+
+                </div>
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+    )
+}
